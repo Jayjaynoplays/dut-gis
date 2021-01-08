5993578e0a54c98c432fa007820195e5f8daf090
Jayjaynoplays <nhenh2712@gmail.com>
2021-01-08 17:28:43 +0700

    add animation for navbar background

--- mysandbox/script.js
+++ mysandbox/script.js
@@ -1,5 +1,19 @@
+var x = document.getElementById("navbar");
+
 function openNav() {
-  var x = document.getElementById("navbar");
   if (x.className == "navbar--container") x.className += " responsive";
   else x.className = "navbar--container";
 }
+
+window.addEventListener("scroll", () => {
+  var y = document.getElementById("navbar");
+  const scrollFromTop = window.pageYOffset;
+  console.log(scrollFromTop);
+  if (scrollFromTop > 10) {
+    y.style.backgroundColor = "rgb(33,40,50)";
+  } else {
+    y.style.backgroundColor = "transparent";
+    y.style.transition= "background 0.7s ease";
+    
+  }
+});
