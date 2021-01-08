62685cd74a1a8c40ecaefcd6dfbd65b5698f56b4
Jayjaynoplays <48757266+Jayjaynoplays@users.noreply.github.com>
2021-01-08 23:43:09 +0700

    update js for home section

--- mysandbox/script.js
+++ mysandbox/script.js
@@ -4,26 +4,27 @@ function openNav() {
   else x.className = "navbar--container";
 }
 
-window.addEventListener("load", () => {
-  var z = document.getElementById("home--contentA");
-  var e = document.getElementById("home--imageA");
-  var f = document.getElementById("home--content--container");
-  window.setTimeout(() => {
-    f.style.display="flex";
-    z.style.animation = "floatBubble 0.6s";
-  }, 800);
-  e.style.display = "block";
-  e.style.animation = "floatBubble 0.6s";
-  e.style.animationDelay = "0.2s";
-});
+if (window.innerWidth > 400){
+  window.addEventListener("load", () => {
+    var z = document.getElementById("home--contentA");
+    var e = document.getElementById("home--imageA");
+    var f = document.getElementById("home--content--container");
+    
+    window.setTimeout(() => {
+      f.style.display="flex";
+      z.style.animation = "floatBubble 0.3s";
+      e.style.animation = "floatBubble 0.45s";
+    }, 250);
+  });  
+}
 
-if (window.innerWidth > 768) {
+if (window.innerWidth > 991.98) {
   window.addEventListener("load", () => {
     window.addEventListener("scroll", () => {
       var y = document.getElementById("navbar");
       const scrollFromTop = window.pageYOffset;
-      console.log(scrollFromTop);
       if (scrollFromTop > 50) {
+        console.log(scrollFromTop)
         y.style.backgroundColor = "rgb(33,40,50)";
       } else {
         y.style.backgroundColor = "transparent";
