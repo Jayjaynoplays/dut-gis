a5d8b7e527b14b9f1f44d6334c553a7b51411ea3
Jayjaynoplays <nhenh2712@gmail.com>
2021-01-08 15:58:57 +0700

    clean all before make some animation

--- mysandbox/script.js
+++ mysandbox/script.js
@@ -1,11 +1,5 @@
 function openNav() {
   var x = document.getElementById("navbar");
-  var y = document.getElementById("navbar--compo");
-  if (x.className == "navbar--container") {
-    x.className += " responsive";
-    y.className += " responsive";
-  } else {
-    x.className = "navbar--container";
-    y.className = "navbar--compo";
-  }
+  if (x.className == "navbar--container") x.className += " responsive";
+  else x.className = "navbar--container";
 }
