1d97ea14cd0f891e999a75868d9c9c06bcbace33
Jayjaynoplays <nhenh2712@gmail.com>
2021-01-13 10:53:58 +0700

    no limit screen size for home effect

--- scripts/home.js
+++ scripts/home.js
@@ -4,36 +4,34 @@ function openNav() {
   else x.className = "navbar--container";
 }
 
-if (window.innerWidth > 400) {
-  window.addEventListener("load", () => {
-    var z = document.getElementById("home--contentA");
-    var e = document.getElementById("home--imageA");
-    var f = document.getElementById("home--content--container");
-    window.setTimeout(() => {
-      f.style.display = "flex";
-      z.style.animation = "floatBubble";
-      e.style.animation = "floatBubble";
-      z.style.animationDuration = "0.3s";
-      e.style.animationDuration = "0.45s";
-    }, 700);
-  });
+window.addEventListener("load", () => {
+  var z = document.getElementById("home--contentA");
+  var e = document.getElementById("home--imageA");
+  var f = document.getElementById("home--content--container");
+  window.setTimeout(() => {
+    f.style.display = "flex";
+    z.style.animation = "floatBubble";
+    e.style.animation = "floatBubble";
+    z.style.animationDuration = "0.3s";
+    e.style.animationDuration = "0.45s";
+  }, 700);
+});
 
-  window.addEventListener("scroll", () => {
-    var z = document.getElementById("home--contentA");
-    var e = document.getElementById("home--imageA");
-    var f = document.getElementById("home--content--container");
-    const scrollFromTop = window.pageYOffset;
-    if (scrollFromTop >= 400) {
-      f.style.display = "none";
-    } else {
-      f.style.display = "flex";
-      z.style.animation = "floatBubble";
-      e.style.animation = "floatBubble";
-      z.style.animationDuration = "0.3s";
-      e.style.animationDuration = "0.45s";
-    }
-  });
-}
+window.addEventListener("scroll", () => {
+  var z = document.getElementById("home--contentA");
+  var e = document.getElementById("home--imageA");
+  var f = document.getElementById("home--content--container");
+  const scrollFromTop = window.pageYOffset;
+  if (scrollFromTop >= 400) {
+    f.style.display = "none";
+  } else {
+    f.style.display = "flex";
+    z.style.animation = "floatBubble";
+    e.style.animation = "floatBubble";
+    z.style.animationDuration = "0.3s";
+    e.style.animationDuration = "0.45s";
+  }
+});
 
 if (window.innerWidth > 991.98) {
   window.addEventListener("load", () => {
